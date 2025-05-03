f461ca661c7f7bf443054f558b9f067ce9c997de
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-05-03 17:06:17 +0700

    fix(config): add missing auth token and export loadConfig()
    - Add missing TURSO_AUTH_TOKEN for production environment
    - Wrap loading environment variable functionality inside loadConfig()
      function, and both execute in the same file, env.js
    - Export loadConfig() for using with external tools, e.g., Drizzle Kit,
      which does not run via dev and start script in package.json

--- src/config/env.js
+++ src/config/env.js
@@ -1,24 +1,33 @@
 import { randomBytes } from 'crypto';
+import path from 'path';
 
 export let NODE_ENV;
 
-try {
-  process.loadEnvFile();
-  NODE_ENV = process.env.NODE_ENV ?? 'development';
-  process.loadEnvFile(
-    NODE_ENV === 'development' ? '.env.local' : '.env.production',
-  );
-} catch (error) {
-  console.error('Cannot load environment variables');
-}
+export const loadConfig = () => {
+  try {
+    process.loadEnvFile();
+    NODE_ENV = process.env.NODE_ENV ?? 'development';
+    process.loadEnvFile(
+      NODE_ENV === 'development' ? '.env.local' : '.env.production',
+    );
+    console.log(`Loaded ${NODE_ENV} environment variables`);
+  } catch (error) {
+    console.error('Unable to load environment variables');
+  }
+};
+loadConfig();
 
 export const config = {
   CORS_ALLOW_ORIGIN: process.env.CORS_ALLOW_ORIGIN ?? '*',
   CORS_CREDENTIALS: Boolean(process.env.CORS_CREDENTIALS) ?? false, // NOTE: set `true` to use cookies
   HOSTNAME: process.env.HOSTNAME ?? 'http://127.0.0.1',
-  MONGO_URI: process.env.MONGO_URI ?? 'mongodb://localhost:27017',
-  PORT: process.env.PORT ?? 3000,
-  TURSO_URI: process.env.TURSO_URI ?? 'sqlite3:test.sqlite3',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN ?? '3h',
   JWT_SECRET: process.env.JWT_SECRET ?? randomBytes(64).toString('hex'),
+  MONGO_URI: process.env.MONGO_URI ?? 'mongodb://localhost:27017',
+  PORT: process.env.PORT ?? 3000,
+  TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
+  TURSO_URI:
+    NODE_ENV === 'development' ?
+      `file:///${path.resolve(process.env.TURSO_URI ?? 'db.sqlite3')}`
+    : process.env.TURSO_URI,
 };
