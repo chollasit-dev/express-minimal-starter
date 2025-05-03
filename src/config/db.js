6e689d10f52d04c270eb9cf9079a4d7b33b6a16e
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-05-03 15:57:38 +0700

    db(config): implement SQLite3 and Turso connection
    Add SQLite3 connection for development environment and Turso for
    production environment.

---
+++ src/config/db.js
@@ -0,0 +1,29 @@
+import { sql } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/libsql';
+import { config, NODE_ENV } from '../config/env.js';
+
+export const db = drizzle({
+  connection: {
+    url: config.TURSO_URI,
+    authToken: config.TURSO_AUTH_TOKEN,
+  },
+});
+
+const pingConnection = async () => {
+  const result = await db.run(sql`SELECT 1;`);
+  if (!result.rows.length) {
+    console.error('[Database]: Unable to ping to the database');
+    process.exit(1);
+  }
+  console.log(
+    `[Database]: ${NODE_ENV === 'development' ? 'Test' : 'Ping'} connection successfully`,
+  );
+};
+
+export const initializeDb = async () => {
+  await pingConnection();
+  if (NODE_ENV === 'development') {
+    await db.run(sql`PRAGMA foreign_keys = 1;`);
+    console.log('[Database]: Foreign keys enabled');
+  }
+};
