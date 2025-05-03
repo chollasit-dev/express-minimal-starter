d2e24ac37799e0d95c1d85205debdbd8c9b77b06
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-05-03 15:57:38 +0700

    feat(db): implement SQLite3 and Turso connection
    Add SQLite3 connection for development environment and Turso for
    production environment.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import { app } from './app.js';
+import { initializeDb } from './config/db.js';
 import { config } from './config/env.js';
 
-const { HOSTNAME, PORT } = config;
+await initializeDb();
 
+const { HOSTNAME, PORT } = config;
 app.listen(PORT, (err) => {
   if (err) {
     console.error(err.message);
