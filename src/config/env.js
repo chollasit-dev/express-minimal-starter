d5860e8a98f737fe40b5e489e05627492ea70232
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-05-19 17:34:02 +0700

    refactor(config): simplify environment variable loader
    Simplify environment variable configuration logic by
    
    - Modify execute flow to immediately exit when loading failed rather
      than relying on hard-coded default values
    - Add logging before NODE_ENV load for ease of debugging

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,4 @@
 import { randomBytes } from 'crypto';
-import path from 'path';
 
 export let NODE_ENV;
 
@@ -7,27 +6,38 @@ export const loadConfig = () => {
   try {
     process.loadEnvFile();
     NODE_ENV = process.env.NODE_ENV ?? 'development';
+    console.log(`[Server]: NODE_ENV loaded`);
     process.loadEnvFile(
       NODE_ENV === 'development' ? '.env.local' : '.env.production',
     );
     console.log(`[Server]: ${NODE_ENV} environment variables loaded`);
   } catch (error) {
     console.error('[Server]: Unable to load environment variables');
+    process.exit(1);
   }
 };
 loadConfig();
 
 export const config = {
-  CORS_ALLOW_ORIGIN: process.env.CORS_ALLOW_ORIGIN ?? '*',
-  CORS_CREDENTIALS: Boolean(process.env.CORS_CREDENTIALS) ?? false, // NOTE: set `true` to use cookies
-  HOSTNAME: process.env.HOSTNAME ?? 'http://127.0.0.1',
-  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN ?? '3h',
+  // Server
+  HOSTNAME: process.env.HOSTNAME,
+  PORT: process.env.PORT,
+
+  // Cors
+  CORS_ALLOW_ORIGIN:
+    // WARN: Only use `,` for separating multiple URLs,
+    process.env.CORS_ALLOW_ORIGIN?.includes(',') ?
+      process.env.CORS_ALLOW_ORIGIN.split(',')
+    : process.env.CORS_ALLOW_ORIGIN,
+  // NOTE: set `true` to use cookies
+  CORS_CREDENTIALS: process.env.CORS_CREDENTIALS,
+
+  // Auth
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
   JWT_SECRET: process.env.JWT_SECRET ?? randomBytes(64).toString('hex'),
-  MONGO_URI: process.env.MONGO_URI ?? 'mongodb://localhost:27017',
-  PORT: process.env.PORT ?? 3000,
+
+  // DB
+  MONGO_URI: process.env.MONGO_URI,
   TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
-  TURSO_DATABASE_URL:
-    NODE_ENV === 'development' ?
-      `file:///${path.resolve(process.env.TURSO_DATABASE_URL ?? 'db.sqlite3')}`
-    : process.env.TURSO_DATABASE_URL,
+  TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
 };
